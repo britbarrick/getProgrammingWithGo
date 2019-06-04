20d812bf7b79bf7c2ec02997bf1d7db9f1cd3c14
Brit Barrick <brittneybarrick@gmail.com>
2019-06-04 15:16:15 -0500

    EOD commit

---
+++ 1ImerativeProgramming/1.2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const distance = 56000000
+	const hoursPerDay = 24
+
+	var days = 28
+
+	fmt.Println(distance/(days*hoursPerDay), "km/h")
+}
---
+++ 1ImerativeProgramming/1.3/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	myNum := 37
+
+	for {
+		compNum := rand.Intn(100)
+		if myNum > compNum {
+			fmt.Println(compNum, "is too small")
+		} else if myNum < compNum {
+			fmt.Println(compNum, "is too big")
+		} else {
+			fmt.Println(compNum, "was the number!")
+			break
+		}
+	}
+}
