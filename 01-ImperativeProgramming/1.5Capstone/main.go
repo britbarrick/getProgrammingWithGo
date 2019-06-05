0ea36439309e7585084868f5fdc3131b8e57e999
Brit Barrick <brittneybarrick@gmail.com>
2019-06-05 09:09:46 -0500

    finished unit 1

---
+++ 01-ImperativeProgramming/1.5Capstone/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const secondsPerDay = 86400
+
+func main() {
+	distance := 62100000
+	spaceline := ""
+	tripType := ""
+
+	fmt.Println("Spaceline       Days  Trip type  Price")
+	fmt.Println("======================================")
+
+	for count := 10; count > 0; count-- {
+		switch rand.Intn(3) {
+		case 0:
+			spaceline = "Space Adventures"
+		case 1:
+			spaceline = "SpaceX"
+		case 2:
+			spaceline = "Virgin Galactic"
+		}
+
+		speed := rand.Intn(15) + 16
+		tripLength := distance / speed / secondsPerDay
+		price := speed + 20.0
+
+		if rand.Intn(2) == 1 {
+			tripType = "Round-trip"
+			price = price * 2
+		} else {
+			tripType = "One-way"
+		}
+		fmt.Printf("%-16v %4v %-10v $%4v \n", spaceline, tripLength, tripType, price)
+	}
+}
