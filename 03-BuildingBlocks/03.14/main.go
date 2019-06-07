9e5826b7a41347929a85fbc7a08404b7823a3941
Brit Barrick <brittneybarrick@gmail.com>
2019-06-07 11:06:48 -0500

    finished 3.14

--- 03-BuildingBlocks/03.14/main.go
+++ 03-BuildingBlocks/03.14/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
+type kelvin float64
+
+// sensor function type
+type sensor func() kelvin
+
+func realSensor() kelvin {
+	return 0
+}
+
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+
+func calibrate(s sensor, offset kelvin) sensor {
+	return func() kelvin {
+		return s() + offset
+	}
+}
+
 func main() {
+	var off kelvin = 12
+	sensor := calibrate(realSensor, off)
+	fmt.Println(sensor())
+	off++
+	fmt.Println(sensor())
+
+	sensor1 := calibrate(fakeSensor, 5)
+	fmt.Println(sensor1())
+	fmt.Println(sensor1())
+	fmt.Println(sensor1())
+	fmt.Println(sensor1())
 
 }
