50f4e8639200d035209bf2678b51c6e4c7bcecce
Brit Barrick <brittneybarrick@gmail.com>
2019-06-10 09:56:04 -0500

    finished 4.18

--- 04-Collections/04.18/main.go
+++ 04-Collections/04.18/main.go
@@ -1,5 +1,15 @@
+// A Bigger Slice
 package main
 
-func main() {
+import "fmt"
 
+type number []int
+
+func main() {
+	var num number
+
+	for i := 0; i < 20; i++ {
+		num = append(num, i)
+		fmt.Println(cap(num), num)
+	}
 }
