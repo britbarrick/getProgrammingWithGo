3ad8403f319e2ff2a2039faaed97e771af076977
Brit Barrick <brittneybarrick@gmail.com>
2019-06-10 14:59:50 -0500

    finished 5.21

--- 05-StateAndBehavior/05.21/main.go
+++ 05-StateAndBehavior/05.21/main.go
@@ -1,5 +1,34 @@
+// A Little Structure
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
+func main() {
+	type location struct {
+		Name string  `json:"landing site"`
+		Lat  float64 `json:"latitude"`
+		Long float64 `json:"longitude"`
+	}
+
+	locations := []location{
+		{"Bradbury Landing", -4.5895, 137.4417},
+		{"Columbia Memorial Station", -14.5684, 175.472636},
+		{"Challenger Memorial Station", -1.9462, 354.4734},
+	}
+
+	bytes, err := json.MarshalIndent(locations, "", " ")
+	exitOnError(err)
+
+	fmt.Println(string(bytes))
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
