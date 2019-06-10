4b6cd8bd0e670cae38070b0265726fd0ea0ddaa9
Brit Barrick <brittneybarrick@gmail.com>
2019-06-10 08:36:52 -0500

    finished 4.17

--- 04-Collections/04.17/main.go
+++ 04-Collections/04.17/main.go
@@ -1,5 +1,20 @@
+// Slices: Windows into Arrays
 package main
 
-func main() {
+import "fmt"
 
+type planets []string
+
+func (p planets) terraform() {
+	for i := range p {
+		p[i] = "New " + p[i]
+	}
+}
+
+func main() {
+	var planets planets = []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptue"}
+
+	planets[3:4].terraform()
+	planets[6:].terraform()
+	fmt.Println(planets)
 }
