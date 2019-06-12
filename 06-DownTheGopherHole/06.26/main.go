faa968bbf2cb4f41bc1b737f1b0332dc98d75bfb
Brit Barrick <brittneybarrick@gmail.com>
2019-06-12 10:56:58 -0500

    finished 6.26

--- 06-DownTheGopherHole/06.26/main.go
+++ 06-DownTheGopherHole/06.26/main.go
@@ -1,5 +1,48 @@
+// A Few Pointers
 package main
 
+import "fmt"
+
+type turtle struct {
+	x, y int
+}
+
+func (t *turtle) up() {
+	t.y++
+}
+
+func (t *turtle) down() {
+	t.y--
+}
+
+func (t *turtle) right() {
+	t.x++
+}
+
+func (t *turtle) left() {
+	t.x--
+}
+
 func main() {
+	turtle := turtle{
+		x: 4,
+		y: 5,
+	}
+
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.down()
+	turtle.left()
+	turtle.left()
+	turtle.left()
+	turtle.right()
+	turtle.up()
+
+	fmt.Println(turtle)
 
 }
