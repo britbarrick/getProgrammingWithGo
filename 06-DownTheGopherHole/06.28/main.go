039cfb2362f5546732cf3c600e76e5c6613846af
Brit Barrick <brittneybarrick@gmail.com>
2019-06-13 11:24:43 -0500

    finished 6.28

--- 06-DownTheGopherHole/06.28/main.go
+++ 06-DownTheGopherHole/06.28/main.go
@@ -1,5 +1,25 @@
+// To Err is Human
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/url"
+	"os"
+)
 
+func main() {
+	u, err := url.Parse("nah")
+
+	if err != nil {
+		fmt.Println(err)
+		fmt.Printf("%#v\n", err)
+
+		if e, ok := err.(*url.Error); ok {
+			fmt.Println("Op:", e.Op)
+			fmt.Println("URL:", e.URL)
+			fmt.Println("Err:", e.Err)
+		}
+		os.Exit(1)
+	}
+	fmt.Println(u)
 }
